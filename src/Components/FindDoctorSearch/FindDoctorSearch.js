70821489da2f02025cb8f8b528f3883e3fb4ed4e
PispirispisS <fedeprzycho@hotmail.com>
2024-07-02 12:28:37 -0400

    FindDoctorSearch component

---
+++ src/Components/FindDoctorSearch/FindDoctorSearch.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import './FindDoctorSearch.css';
+import { useNavigate } from 'react-router-dom';
+
+const initSpeciality = [
+    'Dentist', 'Gynecologist/obstetrician', 'General Physician', 'Dermatologist', 'Ear-nose-throat (ent) Specialist', 'Homeopath', 'Ayurveda'
+];
+
+const FindDoctorSearch = () => {
+    const [doctorResultHidden, setDoctorResultHidden] = useState(true);
+    const [searchDoctor, setSearchDoctor] = useState('');
+    const navigate = useNavigate();
+
+    const handleDoctorSelect = (speciality) => {
+        setSearchDoctor(speciality);
+        setDoctorResultHidden(true);
+        navigate(`/instant-consultation?speciality=${speciality}`);
+    };
+
+    const handleBlur = () => {
+        setTimeout(() => {
+            setDoctorResultHidden(true);
+        }, 100);
+    };
+
+    return (
+        <div className='finddoctor'>
+            <center>
+                <h1>Find a doctor and Consult instantly</h1>
+                <div className="home-search-container">
+                    <div className="doctor-search-box">
+                        <input
+                            type="text"
+                            className="search-doctor-input-box"
+                            placeholder="Search doctors, clinics, hospitals, etc."
+                            onFocus={() => setDoctorResultHidden(false)}
+                            onBlur={handleBlur}
+                            value={searchDoctor}
+                            onChange={(e) => setSearchDoctor(e.target.value)}
+                        />
+                        <div className="search-doctor-input-results" hidden={doctorResultHidden}>
+                            {
+                                initSpeciality.map(speciality => (
+                                    <div className="search-doctor-result-item" key={speciality} onMouseDown={() => handleDoctorSelect(speciality)}>
+                                        <span>{speciality}</span>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </div>
+                </div>
+            </center>
+        </div>
+    );
+};
+
+export default FindDoctorSearch;
