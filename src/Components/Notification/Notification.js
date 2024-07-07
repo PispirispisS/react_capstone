8e2af66d83f198c539e10e4619f571090fbf52d2
PispirispisS <fedeprzycho@hotmail.com>
2024-07-07 10:20:58 -0400

    mejora componente notification

--- src/Components/Notification/Notification.js
+++ src/Components/Notification/Notification.js
@@ -1,30 +1,43 @@
-import React, { useEffect, useState } from 'react';
-import './Notification.css'; // Asegúrate de tener estilos CSS adecuados para la notificación
+import React, { useState, useEffect } from 'react';
+import './Notification.css';
 
 const Notification = ({ appointmentDetails, onCancel }) => {
   const [isVisible, setIsVisible] = useState(false);
+  const [showConfirmation, setShowConfirmation] = useState(false);
 
   useEffect(() => {
     if (appointmentDetails) {
       setIsVisible(true);
 
-      // Ocultar la notificación después de 5 segundos
       const timeout = setTimeout(() => {
         setIsVisible(false);
-      }, 5000);
+        setShowConfirmation(false); // Asegurar que la confirmación esté oculta al volver a mostrar la notificación
+      }, 15000);
 
       return () => clearTimeout(timeout);
     }
   }, [appointmentDetails]);
 
   const handleCancelAppointment = () => {
-    setIsVisible(false);
-    onCancel(); // Llama a la función para cancelar la cita
+    setIsVisible(false); // Ocultar la notificación inicial
+    setTimeout(() => {
+      setShowConfirmation(true); // Mostrar la confirmación de cancelación con un pequeño retardo
+    }, 300); // Ajusta el retardo según sea necesario para que se muestre correctamente
+  };
+
+  const handleConfirmCancel = (confirmed) => {
+    setShowConfirmation(false); // Ocultar la confirmación
+
+    if (confirmed) {
+      onCancel(); // Llamar a la función para cancelar la cita
+    } else {
+      setIsVisible(true); // Mostrar nuevamente la notificación inicial si se decide no cancelar
+    }
   };
 
   return (
-    <div className={`notification-container ${isVisible ? 'show' : ''}`}>
-      {appointmentDetails && (
+    <div className={`notification-container ${isVisible || showConfirmation ? 'show' : ''}`}>
+      {isVisible && !showConfirmation && (
         <div className="notification-content">
           <h3>Appointment Booked!</h3>
           <p>
@@ -37,6 +50,15 @@ const Notification = ({ appointmentDetails, onCancel }) => {
             <strong>Time:</strong> {appointmentDetails.appointmentTime}
           </p>
           <button onClick={handleCancelAppointment}>Cancel Appointment</button>
+        </div>
+      )}
+      {showConfirmation && (
+        <div className="confirmation-message">
+          <h3>Are you sure?</h3>
+          <div className="confirmation-buttons">
+            <button onClick={() => handleConfirmCancel(true)}>Yes</button>
+            <button onClick={() => handleConfirmCancel(false)}>No</button>
+          </div>
         </div>
       )}
     </div>
